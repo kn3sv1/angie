07e9bc644f415ccd733bc64d35c0a69eeb9c6f8c
RSatanovskyi <satcms@hotmail.com>
2022-02-13 11:12:04 +0200

    fix function path

---
+++ lesson29_cats_color_14_02_2022/cats/test/index.php
@@ -0,0 +1,7 @@
+<?php
+//step1: here we should get $person from array
+include_once "../../function.php";
+
+// http://html5.local/angie/lesson29_cats_color_14_02_2022/cats/test/
+
+print_r(getCats());
