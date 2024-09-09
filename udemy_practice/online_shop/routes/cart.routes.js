9b6caff1b646f3019bd564381528c082a174f83d
RSatanovskyi <satcms@hotmail.com>
2024-09-09 11:44:03 +0300

    Added a cart page, modifications in the cart model, cart controller and routes for updating cart items (changing quantities).

--- udemy_practice/online_shop/routes/cart.routes.js
+++ udemy_practice/online_shop/routes/cart.routes.js
@@ -4,7 +4,11 @@ const cartController = require('../controllers/cart.controller');
 
 const router = express.Router();
 
+router.get('/', cartController.getCart); // /cart/
+
 router.post('/items', cartController.addCartItem); // /cart/items
+
+router.patch('/items', cartController.updateCartItem); // /cart/items
 
 
 module.exports = router;
