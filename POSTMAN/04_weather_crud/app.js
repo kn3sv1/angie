9bebb3225659d2907a4c9b16aa84d511a2eb2488
RSatanovskyi <satcms@hotmail.com>
2024-12-07 19:04:09 +0200

    PATCH

---
+++ POSTMAN/04_weather_crud/app.js
@@ -0,0 +1,83 @@
+var http = require('http');
+var url = require('url');
+const { convertChunksToObject } = require('./convertChunksToObject');
+
+function parseDataMiddleware(req, res) {
+    const chunks = [];
+    req.on("data", (chunk) => {
+        chunks.push(chunk);
+    });
+    req.on("end", () => {
+        req.parsedBody = convertChunksToObject(req, chunks);
+        startProcess(req, res)
+    });
+}
+
+http.createServer(parseDataMiddleware).listen(5000);
+
+let weatherCities = [
+    {
+        city: "Limassol",
+        temperature: 17,
+        humidity: 86,
+        wind_speed: 6,
+        air_quality: "fair",
+        icon: "sun",
+    },
+    {
+        city: "Paphos",
+        temperature: 20,
+        humidity: 80,
+        wind_speed: 11,
+        air_quality: "moderate",
+        icon: "sun with clouds",
+    },
+];
+function validCity(newCity) {
+    return !weatherCities.find((allCities) => allCities.city === newCity.city);
+}
+
+function sendErrorMessage(res, errorText) {
+    res.writeHead(400, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(
+        {
+            message: errorText,
+        }));
+}
+
+function startProcess(req, res) {
+    if (req.method === 'POST') {
+        if (validCity(req.parsedBody)) {
+            weatherCities.push(req.parsedBody);
+        } else {
+            sendErrorMessage(res, "City already exists");
+            return;
+        }
+    }
+    if (req.method === 'PATCH') {
+        // 1 step get city
+        // 2 step update object
+        let q = url.parse(req.url, true).query;
+        if (!q.city?.trim()) {
+            sendErrorMessage(res, "City not provided");
+            return;
+        }
+        let cityFoundIndex = weatherCities.findIndex(function (weather) { return weather.city.toLowerCase() === q.city.toLowerCase(); });
+        console.log(`cityFoundIndex = ${cityFoundIndex}`);
+        if (cityFoundIndex === -1) {
+            sendErrorMessage(res, "City not found");
+            return;
+        }
+        // here we always have city
+        weatherCities[cityFoundIndex] = req.parsedBody;
+    }
+
+     // My test case is all in this function
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(
+        {
+            message: "ChUNKS are logged in console",
+            parsedBody: req.parsedBody,
+            weatherCities: weatherCities,
+        }));
+}
---
+++ POSTMAN/04_weather_crud/convertChunksToObject.js
@@ -0,0 +1,47 @@
+// https://frontendguruji.com/blog/how-to-parse-post-request-in-node-js-without-expressjs-body-parser/
+
+function convertChunksToObject(req, chunks) {
+    // Buffer is used to handle the binary streams of data. Binary/hexadecimal array of chunks.
+    const data = Buffer.concat(chunks);
+    //console.log("Data: ", data);
+
+    // We can convert it to string to get a readable format.
+    const stringData = data.toString();
+    //console.log("stringData: ", stringData);
+    let contentType = req.headers['content-type'];
+    if (contentType === 'application/json') {
+        return  JSON.parse(stringData);
+    }
+
+    // THIS BELOW ONLY FOR FORM
+
+    // stringData:  name=Angie&surname=Neophytou we convert "URLSearchParams"
+    // URLSearchParams - google what it is.
+    const parsedData = new URLSearchParams(stringData);
+
+    //console.log("parsedData: ", parsedData);
+    // https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
+    // Convert URLSearchParams object to our own plain object
+    //Copy key and value from "URLSearchParams" to "dataObj"
+    const dataObj = {};
+    for (var pair of parsedData.entries()) {
+        let key = pair[0];
+        dataObj[key] = pair[1];
+    }
+    //console.log("DataObj: ", dataObj);
+
+    // let f = { name: 'Angie', surname : 'Neophytou' };
+    // for (var p of Object.entries(f)) {
+    //     console.log("p: ", p);
+    // }
+    //  // Result of work:
+    // // p:  [ 'name', 'Angie' ]
+    // // p:  [ 'surname', 'Neophytou' ]
+
+
+    return dataObj;
+}
+
+module.exports = {
+    convertChunksToObject: convertChunksToObject,
+};
