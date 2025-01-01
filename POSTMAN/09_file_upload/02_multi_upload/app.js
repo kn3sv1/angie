12e826fc1a3765024bd3fc05553103ae31a58f87
RSatanovskyi <satcms@hotmail.com>
2025-01-01 16:24:22 +0200

    text field - add validation

--- POSTMAN/09_file_upload/02_multi_upload/app.js
+++ POSTMAN/09_file_upload/02_multi_upload/app.js
@@ -6,6 +6,14 @@ http.createServer(function (req, res) {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
+            console.log('fields:', fields);
+            console.log('files:', files);
+
+            // Safe Property Access in JavaScript - not needed here in this example
+            // but we use anyway to see how it works.
+            console.log('firstname:', fields?.firstname[0]);
+            console.log('lastname:', fields?.lastname[0]);
+
             // To see property
             // console.log(files);
             // I added - [0] when I saw that array exists inside property.
@@ -14,22 +22,26 @@ http.createServer(function (req, res) {
             //we use fs.copyFileSync because we want to send multiple files. If two or more files
             // its big problem with callback - callbacks execute randomly in order and we need to handle res.end()
             // res.end() should be called after all uploads.
-            for (const file of files.filetoupload) {
-                console.log("filepath:", file.filepath);
+            // Safe Property Access in JavaScript - if we don't submit file we need this to check if this
+            // property exists.
+            if (files?.filetoupload) {
+                for (const file of files.filetoupload) {
+                    console.log("filepath:", file.filepath);
 
-                var oldpath = file.filepath;
-                console.log('oldpath:', oldpath);
-                var newpath = __dirname + '\\uploaded\\' + 'upload_' + file.originalFilename;
-                try {
-                    fs.copyFileSync(oldpath, newpath);
-                    res.write('File uploaded:' + 'upload_' + file.originalFilename);
-                    res.write("\n");
-                } catch(err) {
-                    throw err;
+                    var oldpath = file.filepath;
+                    console.log('oldpath:', oldpath);
+                    var newpath = __dirname + '\\uploaded\\' + 'upload_' + file.originalFilename;
+                    try {
+                        fs.copyFileSync(oldpath, newpath);
+                        res.write('File uploaded:' + 'upload_' + file.originalFilename);
+                        res.write("\n");
+                    } catch(err) {
+                        throw err;
+                    }
                 }
             }
             //we should have only once with index 0 - files.filetoupload[0]
-            res.end();
+            res.end('END');
 
             // old code - for the first file
 
@@ -47,11 +59,14 @@ http.createServer(function (req, res) {
     } else {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="text" name="firstname"><br>');
+        res.write('<input type="text" name="lastname"><br>');
         res.write('<input type="file" name="filetoupload"><br>');
         // we added second input field to send two files the same time.
         res.write('<input type="file" name="filetoupload"><br>');
-res.write('<input type="submit">');
-res.write('</form>');
-return res.end();
+        res.write('<input type="submit">');
+        res.write('</form>');
+
+        return res.end();
 }
 }).listen(8080);
